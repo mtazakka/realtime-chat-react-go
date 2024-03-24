8fdba5d386ee68dfab491c927e7ae892c2fe7a12
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2024-03-24 11:00:21 +0700

    update

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -2,9 +2,18 @@ import Navbar from '../component/navbar/navbar'
 
 const Home = () => {
   return (
-    <div>
-        <Navbar/>
-    </div>
+    <>
+      <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <h1>hello world</h1>
+      </body>
+      </html>
+    </>
   )
 }
 
