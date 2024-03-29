92aabf98b452c0dfa6ab2ab501a119f8fe30cfa5
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2024-03-29 21:51:28 +0700

    Fix : Logout

--- client/src/app/component/navbar/navbar.tsx
+++ client/src/app/component/navbar/navbar.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { signIn, signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 
 const MobileMenu = () => {
@@ -27,7 +26,8 @@ const ProfileDropdown = () => {
   };
   const router = useRouter()
   const Logout = () => {
-    signOut({ callbackUrl: 'http://localhost:3000/' })
+    localStorage.removeItem('token');
+    router.push('/login')
   }
 
   return (
