faed1014cc2bde95b7cdf6036e57ca06e499d5c1
Muhammad Tazakka Ramadhan <108384217+mtazakka@users.noreply.github.com>
2024-04-02 12:54:07 +0700

    Update checkToken.tsx
    Perlu dicoba

--- client/src/app/component/utils/checkToken.tsx
+++ client/src/app/component/utils/checkToken.tsx
@@ -1,52 +1,10 @@
-"use client";
+import { useState} from "react";
 
-import { useRouter } from "next/navigation";
-import { useEffect, useState, useRef } from "react";
-
-// CheckTokenIn untuk kondisi dimana
-// user sudah login dan ingin akses ke page yang harusnya bisa diakses di kondisi belum login
-export function useCheckTokenIn() {
-  const router = useRouter();
-  const [tokenChecked, setTokenChecked] = useState(true);
-  const affectRan = useRef(false);
-
-  useEffect(() => {
-    if (affectRan.current === false) {
-      const cekToken = () => {
-        const token = localStorage.getItem("token");
-        if (token) {
-          alert("not Allowed");
-          router.push("/NoAuthorized");
-        } else {
-          setTokenChecked(false); // Tandai bahwa pengecekan token sudah dilakukan
-        }
-        cekToken();
-        return () => {
-          affectRan.current = true;
-        }; // Set ref ke true agar hanya dijalankan sekali saat mounting
-      };
-    }
-  }, []);
-
-  return tokenChecked;
-}
-
-// CheckTokenOut untuk kondisi dimana
-// user belum login dan ingin akses ke page yang mengharuskan sudah login
 export function useCheckTokenOut() {
-  const router = useRouter();
-  const [tokenChecked, setTokenChecked] = useState(false); // State untuk menandai apakah pengecekan token sudah selesai
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-      router.push("/NoAuthorized");
-    } else {
-      // Jika token tersedia, tandai pengecekan token sebagai selesai
-      setTokenChecked(true);
-    }
-  }, [router]); // Dependensi hanya router, karena hanya diperlukan sekali saat komponen di-mount
-  // Kembalikan nilai tokenChecked agar dapat digunakan di komponen Home
-  return tokenChecked;
+  const token = localStorage.getItem("token");
+  if (!token) {
+      alert ("gagal")
+      return false
+    } 
+  return true
 }
